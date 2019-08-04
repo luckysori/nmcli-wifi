b5e0e6839a9f60c3de25e203447537b234e61934
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-08-05 07:42:30 +1000

    Make all columns sortable

--- nmcli-wifi.el
+++ nmcli-wifi.el
@@ -1,14 +1,14 @@
 (define-derived-mode nmcli-wifi-mode tabulated-list-mode
   "nmcli-wifi"
   "nmcli WiFi Mode"
-  (let ((columns [("IN-USE" 10)
-                  ("SSID" 30)
-                  ("MODE" 10)
-                  ("CHAN" 5)
-                  ("RATE" 15)
-                  ("SIGNAL" 10)
-                  ("BARS" 5)
-                  ("SECURITY" 10)])
+  (let ((columns [("IN-USE" 10 t)
+                  ("SSID" 30 t)
+                  ("MODE" 10 t)
+                  ("CHAN" 5 t)
+                  ("RATE" 15 t)
+                  ("SIGNAL" 10 t)
+                  ("BARS" 5 t)
+                  ("SECURITY" 10 t)])
         (rows (nmcli-wifi--shell-command)))
     (setq tabulated-list-format columns)
     (setq tabulated-list-entries rows)
